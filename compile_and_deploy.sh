607a61928879cc7f3201db72b3e1319715b45847
Luca Parolini <lucapar@gmail.com>
2018-04-22 11:49:12 +0200

    Disabled german language

--- compile_and_deploy.sh
+++ compile_and_deploy.sh
@@ -7,13 +7,21 @@ rc=$?; if [[ $rc != 0 ]]; then cd .. && exit $rc; fi
 cd ..
 
 echo "Compiling website"
+rm -rf public/*
 hugo
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo "Minimizing public folder content"
 css-html-js-minify --overwrite ./public
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo "Adding .htaccess file to public folder"
+cp .htaccess public/
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo "Uploading website"
+rsync -avz --delete public/ a2_website:/home/annmarie/lucaparolini.com
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo "Deployment succesful"
 exit 0
-#
-# echo "Uploading website"
-# exit 0
